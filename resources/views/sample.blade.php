d4cca90b99d5dbc071f7d35068010a7103c79f33
JKawakami-stkn <j.kawakami.sotuken@gmail.com>
2019-09-10 03:37:52 +0900

    ツールチップを表示

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -72,47 +72,50 @@
         <!-- 画像のみカード -->
         <div class="container" >
             <div class="row">
+
                 <div class="col-4">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/purchase.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/purchase.png') }}" data-toggle="tooltip" title="購入">
                     </div>
                 </div>
 
                 <div class="col-4">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/check.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/check.png') }}" data-toggle="tooltip" title="検品">
                     </div>
                 </div>
 
                 <div class="col-4">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/delivery.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/delivery.png') }}" data-toggle="tooltip" title="検品">
                     </div>
                 </div>
+
             </div>
 
             <div class="row">
+            
                 <div class="col-3">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/purchase_confirmation.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/purchase_confirmation.png') }}" data-toggle="tooltip" title="注文状況確認">
                     </div>
                 </div>
 
                 <div class="col-3">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/print.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/print.png') }}" data-toggle="tooltip" title="印刷">
                     </div>
                 </div>
 
                 <div class="col-3">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/dl.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/dl.png') }}" data-toggle="tooltip" title="ダウンロード">
                     </div>
                 </div>
 
                 <div class="col-3">
                     <div class="card card-body">
-                        <img class="card-img" src="{{ asset('img/edit.png') }}" alt="カードの画像">
+                        <img class="card-img" src="{{ asset('img/edit.png') }}" alt="カードの画像" data-toggle="tooltip" title="情報編集">
                     </div>
                 </div>
 
