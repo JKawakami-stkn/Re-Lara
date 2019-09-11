d3c21bc394d4e2a102a7273730e67c9e3d635b17
JKawakami-stkn <j.kawakami.sotuken@gmail.com>
2019-09-12 03:21:53 +0900

    コンテンツを追加するボタンを追加

--- resources/views/sales-list.blade.php
+++ resources/views/sales-list.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<button type="button" class="btn btn-primary rounded-circle p-0 position-fixed" style="width:4rem;height:4rem; bottom:55px; right:20px; z-index:30;">＋</button>
+
 <div class="container" >
 
     <!-- パンくずリスト -->
