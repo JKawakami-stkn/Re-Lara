d3be8d2839c104b9c06ce7e21271d3e225bf6517
JKawakami-stkn <j.kawakamisotuken@gmail.com>
2019-10-16 13:00:45 +0900

    PWAの設定ファイルを作成

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+
+var CACHE_NAME = 're-lara-caches';
+var urlsToCache = [
+'/Re-Lara/',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
