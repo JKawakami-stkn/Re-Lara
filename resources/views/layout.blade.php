aff4172e08a6c287512c6cc6cfd2b1112b45bb01
JKawakami-stkn <j.kawakamisotuken@gmail.com>
2019-11-06 11:45:48 +0900

    ファビコン設定

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
     <!-- フォント -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- CSS -->
