937c8271b0c30deafc7b77f921773374a16f3454
JKawakami-stkn <j.kawakamisotuken@gmail.com>
2019-11-06 11:03:54 +0900

    ヘッダーとwelcomeページのロゴを変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="content">
     <div class="d-flex justify-content-center">
         <div class="d-flex align-items-center display-3" style="height:32rem">
-            Partone
+            <img src="{{ asset('img/partone_logo.png') }}" class="d-inline-block align-top" style="width: auto; height: 75px;">
         </div>
     </div>
 </div>
