5254f13349d9971a79efd67ae53bac6624cb3230
Kawakami <j.kawakami.sotuken@gmail.com>
2019-11-07 07:30:18 +0900

    発注書のレイアウトを編集

--- resources/views/personal-purchase-print.blade.php
+++ resources/views/personal-purchase-print.blade.php
@@ -5,7 +5,7 @@
 @section('style')
 
 @stop
-<div class="container">
+<div class="container" style="min-width:695px;">
 
     <!-- パンくずリスト -->
     <nav aria-label="breadcrumb">
@@ -15,7 +15,7 @@
             <li class="breadcrumb-item active" aria-current="page">注文確認</li>
         </ol>
     </nav>
-    
+
     <div id="carouselExampleControls" class="carousel slide border" data-ride="carousel">
         <!-- スライドさせる画像の設定 -->
         <div class="carousel-inner">
@@ -28,128 +28,127 @@
             </div>
             -->
             <div class="carousel-item active data-interval='0'">
-                <!-- ここから発注書-->
 
-                    <div class="container">
+                <div class="wrapper">
+                    <div class="content">
 
                         <div class="layout">
-                        <div class="day">
-                        </div>
+                            <div class="day">(日付)</div>
 
-                        <div class="number">注文番号：(番号)</div>
+                            <div class="number">注文番号：(番号)</div>
 
-                        <div class="header">
-                            <div class="title">注　文　書</div>
-                            <div class="sub-title">PURCHASE ORDER</div>
-                        </div>
+                            <div class="header">
+                                <div class="title">注　文　書</div>
+                                <div class="sub-title">PURCHASE ORDER</div>
+                            </div>
 
-                        <div class="vender">
-                            <div class="vendor-name"> 取引先名 </div>
-                            <div class="vendor-rep">(取引先名)様</div>
-                        </div>
+                            <div class="vender">
+                                <div class="vendor-name"> 取引先名 </div>
+                                <div class="vendor-rep">(取引先名)様</div>
+                            </div>
 
-                        <div class="office">
-                            <div class="office-name">イートンちどり保育園</div>
-                            <div class="postal-code">〒702-8024</div>
-                            <div class="street-address">岡山市南区浦安南町425-1</div>
-                            <div class="tel">TEL: 086-265-556</div>
-                            <div class="fax">FAX: 086-265-5562</div>
-                            <div class="rep">担当者：(担当者)様</div>
-                        </div>
+                            <div class="office">
+                                <div class="office-name">イートンちどり保育園</div>
+                                <div class="postal-code">〒702-8024</div>
+                                <div class="street-address">岡山市南区浦安南町425-1</div>
+                                <div class="tel">TEL: 086-265-556</div>
+                                <div class="fax">FAX: 086-265-5562</div>
+                                <div class="rep">担当者：(担当者)様</div>
+                            </div>
 
-                        <div class="message"> 以下の通り発注いたします。 </div>
+                            <div class="message"> 以下の通り発注いたします。</div>
 
-                        <table>
-                            <thead>
-                            <tr>
-                                <th class="no">No</th>
-                                <th class="supplie-name">品 番 • 品 名</th>
-                                <th class="num">数 量</th>
-                            </tr>
-                            </thead>
-                            <tbody>
+                            <table>
+                                <thead>
                                 <tr>
-                                    <td class="no">00</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
+                                    <th class="no">No</th>
+                                    <th class="supplie-name">品 番 • 品 名</th>
+                                    <th class="num">数 量</th>
                                 </tr>
-                                <tr>
-                                    <td class="no">01</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>
-                                <tr>
-                                    <td class="no">02</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>                            
-                                <tr>
-                                    <td class="no">03</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">04</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">05</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">06</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">07</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">08</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">09</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">10</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">11</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">12</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>      
-                                <tr>
-                                    <td class="no">13</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr> 
-                                <tr>
-                                    <td class="no">14</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>    
-                                <tr>
-                                    <td class="no">15</td>
-                                    <td class="supplie-name">(商品名 + SKU)</td>
-                                    <td class="num">(数量)</td>
-                                </tr>         
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="no">00</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">01</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">02</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">03</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">04</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">05</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">06</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">07</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">08</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">09</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">10</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">11</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">12</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">13</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">14</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no">15</td>
+                                        <td class="supplie-name">(商品名 + SKU)</td>
+                                        <td class="num">(数量)</td>
+                                    </tr>
 
-                            </tbody>
+                                </tbody>
                             </table>
 
                             <div class="remarks">
