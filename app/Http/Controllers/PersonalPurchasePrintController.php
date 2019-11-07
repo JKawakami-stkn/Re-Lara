12715c503100e5c13f1d89af49671ae2c656a5c9
JKawakami-stkn <j.kawakamisotuken@gmail.com>
2019-11-07 16:35:18 +0900

    情報を表示

--- app/Http/Controllers/PersonalPurchasePrintController.php
+++ app/Http/Controllers/PersonalPurchasePrintController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class PersonalPurchasePrintController extends Controller
 {
-    public function show(){
+    public function show($personal_sale_id){
 
-        return view('personal-purchase-print');
+        $personal_sale = \App\models\Personal_sale::find($personal_sale_id);
+        $personal_orders = $personal_sale->personal_orders;
+        $suppliers = \App\models\Supplier::all();
+        
+        return view('personal-purchase-print', ['personal_sale'=>$personal_sale, 'personal_orders'=>$personal_orders, 'suppliers'=>$suppliers]);
     }
 }
--- resources/views/personal-purchase-print.blade.php
+++ resources/views/personal-purchase-print.blade.php
@@ -5,8 +5,7 @@
 @section('style')
 
 @stop
-<div class="container>
-
+<div class="container">
     <!-- パンくずリスト -->
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb noprint">
@@ -16,127 +15,73 @@
         </ol>
     </nav>
             
-                
-    <div class="layout">
-        <div class="day">(日付)</div>
 
-        <div class="number">注文番号：(番号)</div>
+    @foreach($suppliers as $supplier)
+        <div class="layout">
+            <div class="day">{{ date("Y/m/d") }}</div>
+            <div class="number">注文番号：{{ sprintf('%05d', $personal_sale->id) }}</div>
 
-        <div class="header">
-            <div class="title">注　文　書</div>
-            <div class="sub-title">PURCHASE ORDER</div>
+            <div class="header">
+                <div class="title">注　文　書</div>
+                <div class="sub-title">PURCHASE ORDER</div>
+            </div>
+
+            <div class="vender">
+                <div class="vendor-name"> 取引先名 </div>
+                <div class="vendor-rep">(取引先名)様</div>
+            </div>
+
+            <div class="office">
+                <div class="office-name">イートンちどり保育園</div>
+                <div class="postal-code">〒702-8024</div>
+                <div class="street-address">岡山市南区浦安南町425-1</div>
+                <div class="tel">TEL: 086-265-556</div>
+                <div class="fax">FAX: 086-265-5562</div>
+                <div class="rep">担当者：(担当者名)</div>
+            </div>
+
+            <div class="message"> 以下の通り発注いたします。</div>
+
+            <table>
+                <thead>
+                <tr>
+                    <th class="no">No</th>
+                    <th class="supplie-name">品 番 • 品 名</th>
+                    <th class="num">数 量</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php  $order_loop_index = 0?>
+                    @foreach ($personal_orders as $personal_order)
+                        <!-- 一致するオーダーが存在する -->
+                        @if($supplier->id == \App\models\Supplie::find($personal_order->supplie_id)->supplier_id)
+                            <td class="no">{{ $order_loop_index + 1 }}</td>
+                            <td class="supplie-name">{{ \App\models\Supplie::find($personal_order->supplie_id)->name }}</td>
+                            <td class="num">{{$personal_order->quantity}}</td>
+                            </tr>
+                            <?php  $order_loop_index += 1?>
+                        @endif
+                        {{$order_loop_index}}
+                        <!-- オーダーのループ終了 -->
+                        @if ($order_loop_index == count($personal_orders) )
+                            @for ($i = $order_loop_index + 1; $i < 16; $i++)
+                                <td class="no">{{ $i }}</td>
+                                <td class="supplie-name"></td>
+                                <td class="num"></td>
+                                </tr>
+                            @endfor
+                        @endif
+
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="remarks">
+            備考：
+            </div>
+
         </div>
-
-        <div class="vender">
-            <div class="vendor-name"> 取引先名 </div>
-            <div class="vendor-rep">(取引先名)様</div>
-        </div>
-
-        <div class="office">
-            <div class="office-name">イートンちどり保育園</div>
-            <div class="postal-code">〒702-8024</div>
-            <div class="street-address">岡山市南区浦安南町425-1</div>
-            <div class="tel">TEL: 086-265-556</div>
-            <div class="fax">FAX: 086-265-5562</div>
-            <div class="rep">担当者：(担当者)</div>
-        </div>
-
-        <div class="message"> 以下の通り発注いたします。</div>
-
-        <table>
-            <thead>
-            <tr>
-                <th class="no">No</th>
-                <th class="supplie-name">品 番 • 品 名</th>
-                <th class="num">数 量</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="no">01</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">02</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">03</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">04</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">05</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">06</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">07</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">08</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">09</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">10</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">11</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">12</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">13</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">14</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-                <tr>
-                    <td class="no">15</td>
-                    <td class="supplie-name">(商品名 + SKU)</td>
-                    <td class="num">(数量)</td>
-                </tr>
-
-            </tbody>
-        </table>
-
-        <div class="remarks">
-        備考：
-        </div>
-
-    </div>
-
+    @endforeach
 
     <!-- 印刷ボタン -->
     <input type="button" value="印刷" class="noprint" onclick="window.print();"/>
