8731d37179a27ab0c3b4373b43fe0fd8c5328c6a
JKawakami-stkn <j.kawakamisotuken@gmail.com>
2019-11-08 11:51:07 +0900

    処理を簡略化

--- app/Http/Controllers/PersonalPurchasePrintController.php
+++ app/Http/Controllers/PersonalPurchasePrintController.php
@@ -12,13 +12,19 @@ class PersonalPurchasePrintController extends Controller
 {
     public function show($personal_sale_id){
 
+        // 注文
         $personal_sale = Personal_sale::find($personal_sale_id);
-        $personal_orders = $personal_sale->personal_orders;
-        //$suppliers = Supplier::all();
 
-        // 注文された用品を取り扱う取引先を取得
+        // 注文された用品一覧
+        $personal_orders = $personal_sale->personal_orders;
+
+        // 用品id一覧
         $ordered_supplies_id = Personal_order::where('personal_sale_id', $personal_sale_id)->get('supplie_id');
+
+        // 発注書を送る取引先一覧
         $supplies_id = Supplie::whereIn('id', $ordered_supplies_id)->groupBy('supplier_id')->get('supplier_id');
+        $suppliers = Supplier::find($supplies_id);
+        \Debugbar::info($suppliers);
 
         return view('personal-purchase-print', ['personal_sale'=>$personal_sale, 'personal_orders'=>$personal_orders, 'suppliers'=>$suppliers]);
     }
