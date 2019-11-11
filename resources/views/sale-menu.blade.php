876006d066ffdfeb9c10c8dce54630031f4437ec
今田陵也 <ryoyaimada1215@gmail.com>
2019-11-11 10:47:23 +0900

    販売会新規登録実装

--- resources/views/sale-menu.blade.php
+++ resources/views/sale-menu.blade.php
@@ -9,13 +9,13 @@
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{ route('menu.show') }}">メニュー</a></li>
         <li class="breadcrumb-item"><a href="{{ route('sales') }}">物品販売</a></li>
-        <li class="breadcrumb-item active" aria-current="page">{{ $sale_name }}</li>
+        <li class="breadcrumb-item active" aria-current="page">{{ $sale->name }}</li>
     </ol>
 </nav>
 
 <div class="card _border-0 mb-4">
   <div class="card-body">
-    <h4>{{ $sale_name }}の詳細</h4>
+    <h4>{{ $sale->name }}の詳細</h4>
     <br>
     <p>　期日：</p>
     <p>　対象：</p>
@@ -29,7 +29,7 @@
 
         <!-- 画像のみカード -->
         <div class="col-4">
-            <a href="{{ route('purchase-target', $sale_name) }} ">
+            <a href="{{ route('purchase-target', $sale->id) }} ">
                 <div class="card card-body">
                     <img class="card-img" src="{{ asset('img/purchase.png') }}" data-toggle="tooltip" title="購入">
                 </div>
@@ -37,7 +37,7 @@
         </div>
 
         <div class="col-4">
-            <a href="{{ route('inspection', $sale_name) }}">
+            <a href="{{ route('inspection', $sale->id) }}">
                 <div class="card card-body">
                     <img class="card-img" src="{{ asset('img/check.png') }}" data-toggle="tooltip" title="検品">
                 </div>
@@ -45,7 +45,7 @@
         </div>
 
         <div class="col-4">
-            <a href="{{ route('delivery-target', $sale_name) }}">
+            <a href="{{ route('delivery-target', $sale->id) }}">
                 <div class="card card-body">
                     <img class="card-img" src="{{ asset('img/delivery.png') }}" data-toggle="tooltip" title="引き渡し">
                 </div>
@@ -57,7 +57,7 @@
     <div class="row">
     
         <div class="col-3">
-            <a href="{{ route('order-status', $sale_name) }}">
+            <a href="{{ route('order-status', $sale->id) }}">
                 <div class="card card-body">
                     <img class="card-img" src="{{ asset('img/purchase_confirmation.png') }}" data-toggle="tooltip" title="注文状況確認">
                 </div>
@@ -81,7 +81,7 @@
         </div>
 
         <div class="col-3">
-            <a href="{{ route('sale-edit', $sale_name) }}">
+            <a href="{{ route('sale-edit', $sale->id) }}">
                 <div class="card card-body">
                     <img class="card-img" src="{{ asset('img/edit.png') }}" alt="カードの画像" data-toggle="tooltip" title="情報編集">
                 </div>
--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -51,16 +51,19 @@
     </div>
 
     <!-- カード -->
-    <div class="card">
-        <h5 class="card-header text-center">テスト販売会</h5>
-        <div class="card-body">
-            <h5 class="card-title"></h5>
-            <p class="card-text">
-                期　日：
-            </p>
-            <a href="{{ route('sale-menu', 'テスト販売会') }}" class="btn btn-primary btn-block" style="width:100%;">選択する</a>
+    @foreach ($sales as $sale)
+        <div class="card">
+            <h5 class="card-header text-center">{{$sale->name}}</h5>
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text">
+                    期　日：
+                </p>
+                <a href="{{ route('sale-menu', $sale->id) }}" class="btn btn-primary btn-block" style="width:100%;">選択する</a>
+            </div>
         </div>
-    </div>
+    @endforeach
+    
 </div>
 
 @stop
