0255d533e86d4d9e44cbc1099d615bdabd0a9460
今田陵也 <ryoyaimada1215@gmail.com>
2019-11-19 20:43:55 +0900

    注文確認

---
+++ resources/views/----order-status.blade.php
@@ -0,0 +1,91 @@
+@extends('./layout')
+
+@section('content')
+
+<div class="container">
+
+<!-- パンくずリスト -->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('menu.show') }}">メニュー</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('sales') }}">物品販売</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('sale-menu', $sale->id) }}">{{ $sale->name }}</a></li>
+        <li class="breadcrumb-item active" aria-current="page">注文状況確認</li>
+    </ol>
+</nav>
+
+    <form>
+         <!-- jsでajaxのリンクを取得するため -->
+        <input type="hidden" id="ajax_url" value="{{ route('order-status.load',$sale->id) }}">
+        <div class="row">
+            <div class="col-md mt-3 mb-3">
+                <div class="form-group">
+                    <label for="FormControlSelect">確認したい組</label>
+                    <select class="form-control form-control-lg" id="group">
+                            <option selected disabled="disabled">選択してください</option>
+                            @foreach($groups as $group)
+                                <option value="{{ $group->GP_CD }}">{{ $group->GP_NM }}</option>
+                            @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+    </form>
+
+
+    <div class="accordion" id="accordion2" role="tablist">
+        <div class="card">
+            <div class="card-header" role="tab" id="heading1" style="background-color:#c9c9c9;">
+                <h5 class="mb-0">
+                    <a data-toggle="collapse" class="text-body stretched-link text-decoration-none" href="#collapse1" aria-expanded="true" aria-controls="collapse1"> 園児の名前１ </a>
+                </h5>
+            </div>
+            <div id="collapse1" class="collapse show" role="tabpanel" aria-labelledby="heading1" _data-parent="#accordion2">
+                <div class="card-body">
+                    <table class="table">
+                        <caption>園児の名前１の注文</caption>
+                        <thead class="thead-light">
+                            <tr>
+                                <th></th>
+                                <th scope="col">用品</th>
+                                <th scope="col">数量</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>・</th>
+                                <td>テーブルのセル</td>
+                                <td>810</td>
+                            </tr>
+                            <tr>
+                                <th>・</th>
+                                <td>テーブルのセルテーブルのセル</td>
+                                <td>810</td>
+                            </tr>
+                            <tr>
+                                <th>・</th>
+                                <td>テーブルのセルテーブルのセルテーブルのセルテーブルのセル</td>
+                                <td>810</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+       
+    </div>
+    
+
+
+
+
+
+</div>
+
+@section('script')
+<script src="{{ asset('js/select_child.js') }}"></script>
+@stop
+
+@stop
+
---
+++ resources/views/orders-status.blade.php
@@ -0,0 +1,58 @@
+@extends('./layout')
+
+@section('content')
+
+<div class="container">
+
+    <!-- パンくずリスト -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('menu.show') }}">メニュー</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('sales') }}">物品販売</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('sale-menu', $sale->id) }}">{{ $sale->name }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page">購入</li>
+        </ol>
+    </nav>
+
+    <form action="{{ route('orders-status.store',$sale->id) }}" method="post">
+            {{ csrf_field() }}
+
+             <!-- jsでajaxのリンクを取得するため -->
+        <input type="hidden" id="ajax_url" value="{{ route('orders-status.load',$sale->id) }}">
+
+        <div class="row">
+            <div class="col-md mt-3 mb-3">
+                <div class="form-group">
+                    <label for="FormControlSelect">組の名前</label>
+                    <select class="form-control form-control-lg" id="group">
+                        <option selected disabled="disabled">選択してください</option>
+                        @foreach($groups as $group)
+                            <option value="{{ $group->GP_CD }}">{{ $group->GP_NM }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-md mt-3 mb-3">
+                <div class="form-group">
+                    <label for="FormControlSelect">購入する園児の名前</label>
+                    <select id="kids" class="form-control form-control" name="kids_id" disabled>
+                            <option selected disabled="disabled">選択してください</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+            <div class="mt-5 mb-3">
+                <button type="submit" class="btn btn-primary btn-block">選　　択</button> 
+            </div>
+
+    </form>
+</div>
+
+@section('script')
+<script src="{{ asset('js/select_child.js') }}"></script>
+@stop
+
+
+@stop
+
