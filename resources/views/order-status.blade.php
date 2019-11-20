795e71d87bc74634e5061e7f6c5a7507ab205039
今田陵也 <ryoyaimada1215@gmail.com>
2019-11-20 09:42:36 +0900

    注文確認

--- resources/views/order-status.blade.php
+++ resources/views/order-status.blade.php
@@ -13,14 +13,41 @@
                 <li class="breadcrumb-item active" aria-current="page">購入</li>
             </ol>
      </nav>
+  
 
-     <!-- 園児の名前を表示-->
-    <p>{{$kid->KIDS_NM_KJ}}</p>
-
-    <!-- 用品を表示-->
-    @foreach ($orders as $order)
-    <p>{{$order->name}}</p>
-    @endforeach
+    <div class="accordion" id="accordion2" role="tablist">
+            <div class="card">
+                <div class="card-header" role="tab" id="heading1" style="background-color:#c9c9c9;">
+                    <h5 class="mb-0">
+                        <a data-toggle="collapse" class="text-body stretched-link text-decoration-none" href="#collapse1" aria-expanded="true" aria-controls="collapse1"> {{$kid->KIDS_NM_KJ}} </a>
+                    </h5>
+                </div>
+                <div id="collapse1" class="collapse show" role="tabpanel" aria-labelledby="heading1" _data-parent="#accordion2">
+                    <div class="card-body">
+                        <table class="table">
+                            <!-- 園児の名前を表示-->
+                            <caption>{{$kid->KIDS_NM_KJ}}の注文</caption>
+                            <thead class="thead-light">
+                                <tr>
+                                    <th></th>
+                                    <th scope="col">用品</th>
+                                    <th scope="col">数量</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                  <!-- 用品を表示-->
+                                    @foreach ($orders as $order)
+                                <tr>
+                                    <th>・</th>
+                                    <td>{{$order->name}}</td>
+                                    <td>810</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
     
 
 </div>
