187c57b01c1958b4785c333c122009fa51c90e38
仲田　夏輝 <>
2019-12-06 13:24:39 +0900

    編集と削除したときの不具合対応

--- app/Http/Controllers/PersonalPurchaseCheckController.php
+++ app/Http/Controllers/PersonalPurchaseCheckController.php
@@ -14,18 +14,18 @@ class PersonalPurchaseCheckController extends Controller
         $personal_sku_data = [];
         foreach($personal_sku_ids as $sku_id){
           foreach ($sku_id as $key => $value){
-          $sku_v = \App\models\Sku::where("id", $value)->select("size", "color")->get()->toArray();
+          $sku_v = \App\models\Sku::withTrashed()->where("id", $value)->select("size", "color")->get()->toArray();
           $personal_sku_data[$value] = $sku_v[0];
           }
         }
 
-        // \Debugbar::info($personal_sku_data["1"]["color"]);
-        // \Debugbar::info($personal_orders[0]->sku_id);
+        \Debugbar::info($personal_sku_data);
+        \Debugbar::info($personal_orders[0]->sku_id);
         return view('personal-purchase-check', ['personal_sale' => $personal_sale, 'personal_orders' => $personal_orders, "personal_sku_data" => $personal_sku_data]);
     }
 
     public function store($personal_sale_id){
-        
+
       $personal_sale = \App\models\Personal_sale::find($personal_sale_id);
       $personal_sale->entered_at = date("Y/m/d H:i:s");
       $personal_sale->save();
